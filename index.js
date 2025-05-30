a9bc0d60b73bf9ee3bbf23fc210d463cdee89626
ramirospinheiro <ramiropinheiro97@gmail.com>
2025-05-30 12:04:34 -0300

    Crude completo implementado  e segurança ajustada

--- index.js
+++ index.js
@@ -1,14 +1,15 @@
 const express = require('express')
 const app = express()
 const port = 6579
+require('dotenv').config()
 const {Pool} = require('pg')
 
 const pool = new Pool({
-    user: 'postgres.nkmnlrknimlcuoslodrq',
-    host: 'aws-0-sa-east-1.pooler.supabase.com',
-    database: 'postgres',
-    password: 'banc0dedad0s',
-    port: 5432
+    user: process.env.DB_USER,
+    host: process.env.DB_HOST,
+    database: process.env.DB_NAME,
+    password: process.env.DB_PASS,
+    port: process.env.DB_PORT
 })
 
 app.use(express.json())
@@ -29,17 +30,14 @@ app.post('/produtos', async (req, res) => {
         return res.status(400).send('Categoria pode ter no maximo 50 caracteres')
     }
 
+    // a inserção dos dados no try é indireta para passar pela validação nativa do pg e evitar SQL injection
     try {
-            const produto = await pool.query(`
+        const produto = await pool.query(`
         INSERT INTO produtos (nome, preco, categoria, imagem_url) 
-        VALUES (
-            '${nome}',
-            ${preco},
-            '${categoria}',
-            '${imagem_url}'
-        )   
+        VALUES ($1, $2, $3, $4)   
         RETURNING *
-    `)
+    `, [nome, preco, categoria, imagem_url])
+
     res.status(201).send(produto.rows[0])
     } catch (error) {
         console.error(error)
@@ -55,6 +53,71 @@ app.get('/produtos', async (req, res) => {
     } catch (error) {
         console.error(error)
         return res.status(500).send('Erro ao buscar produtos')
+    }
+})
+
+app.get('/produtos/:id', async (req,res) => {
+    const { id } = req.params;
+    try {
+        const produto = await pool.query(`
+            SELECT * FROM produtos WHERE id = $1
+        `, [id])
+        if (!produto.rows.length){
+            return res.status(404).send('Produto não encontrado')
+        }
+        //res.send() envia o status de sucesso (200) por default, so é necessario definir status se nao for sucesso (200)
+        return res.send(produto.rows[0])
+    } catch (error) {
+        console.error(error)
+        return res.status(500).send('Erro ao buscar produto')
+    }
+})
+
+app.delete('/produtos/:id', async (req, res) => {
+    const {id} = req.params
+
+    try {
+        const produto = await pool.query(`
+            SELECT * FROM produtos WHERE id = $1   
+        `, [id])
+        if (!produto.rows.length){
+            return res.status(404).send('Produto não encontrado')
+        }
+
+        await pool.query(`
+            DELETE FROM produtos WHERE id = $1
+        `, [id])
+        
+        return res.status(204).send('Produto deletado com sucesso')
+    } catch (error) {
+        console.error(error)
+        return res.status(500).send('Erro ao deletar produto')
+    }
+})
+
+app.put('/produtos/:id', async (req,res) => {
+    const {id} = req.params;
+    const {nome, preco, categoria, imagem_url} = req.body;
+
+    try {
+        const produto = await pool.query('SELECT * FROM produtos WHERE id = $1', [id])
+        if (!produto.rows.length){
+            return res.status(404).send('Produto nao encontrado')
+        }
+
+        await pool.query(`
+            UPDATE produtos SET 
+            nome = $1,
+            preco = $2,
+            categoria = $3,
+            imagem_url = $4
+            WHERE id = $5
+        `, [nome, preco, categoria, imagem_url, id])
+
+        return res.send('Produto atualizado com sucesso')
+    } catch (error) {
+        console.error(error)
+        return res.status(500).send('Erro ao atualizar produto')
     }
 })
 
