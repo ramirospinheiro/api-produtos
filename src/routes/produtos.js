e6faf292d8a2a4199d1599f1b5e31cb6b6f6bb58
ramirospinheiro <ramiropinheiro97@gmail.com>
2025-06-09 09:22:37 -0300

    api backend finalizado

--- src/routes/produtos.js
+++ src/routes/produtos.js
@@ -1,15 +1,26 @@
 const express = require('express')
 const router = express.Router()
 const produtosController = require('../controllers/produtos')
-// importar middleware
 const produtosMiddleware = require('../middlewares/produtos')
+const authMiddleware = require('../middlewares/auth')
 
-router.get('/produtos', produtosController.getProdutos)
+router.get('/produtos',
+    authMiddleware.validateToken, 
+    produtosController.getProdutos
+)
 
 //middleware tem que estar no meio, senao vai criar produto antes de validar
-router.post('/produtos', produtosMiddleware.validateCreateProduto, produtosController.createProduto)
+router.post('/produtos',
+    authMiddleware.validateToken,
+    produtosMiddleware.validateCreateProduto,
+    produtosController.createProduto
+)
 
-router.delete('/produtos/:id', produtosMiddleware.validateDeleteProduto, produtosController.deleteProduto)
+router.delete('/produtos/:id',
+    authMiddleware.validateToken,
+    produtosMiddleware.validateDeleteProduto,
+    produtosController.deleteProduto
+)
 
 
 module.exports = router;
